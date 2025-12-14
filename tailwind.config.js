82f0cdbc3150d640e6e40bd328d4b82a4214c1e1
MatoxEdriel <ga99_buggi@hotmail.com>
2025-12-13 23:46:51 -0500

    Implementacion de modulos

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
         "./src/**/*.{html,ts}"
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'techfix-bg': '#F3F8FA',
+            },
+        },
     },
     plugins: [
         require("daisyui")
