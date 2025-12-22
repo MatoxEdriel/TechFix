de9ee759ce4c745c9b7f254c0ebb187072cba25d
MatoxEdriel <ga99_buggi@hotmail.com>
2025-12-21 19:35:36 -0500

    Mejora en cambio de contraseña, visual y logicamente

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './component/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AuthenticationRoutingModule } from './authentication.routing.module';
 import { LayoutComponent } from '../../pages/layout/Layout/Layout.component';
@@ -9,6 +9,7 @@ import { LayoutLoginComponent } from './pages/Layout-login/Layout-login.componen
 import { NgOtpInputComponent, NgOtpInputModule } from 'ng-otp-input';
 import { VerifyCodeComponent } from './pages/verify-code/verify-code';
 import { RecoveryPasswordComponent } from './pages/recovery-password/recovery-password.component';
+import { ResetPasswordComponent } from './component/reset-password/reset-password';
 
 @NgModule({
   imports: [
@@ -18,10 +19,12 @@ import { RecoveryPasswordComponent } from './pages/recovery-password/recovery-pa
     RouterLink,
     AuthenticationRoutingModule,
     RouterOutlet,
-    NgOtpInputModule
+    NgOtpInputModule,
+    FormsModule
+
 
 
   ],
-  declarations: [LoginComponent, LayoutLoginComponent, VerifyCodeComponent, RecoveryPasswordComponent],
+  declarations: [LoginComponent, LayoutLoginComponent, VerifyCodeComponent, RecoveryPasswordComponent, ResetPasswordComponent],
 })
 export class AuthenticationModule { }
