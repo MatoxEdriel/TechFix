851c52fdeab39c98d7d5d57466a63a2178f10fe5
MatoxEdriel <ga99_buggi@hotmail.com>
2025-12-25 23:50:22 -0500

    Implementacion de environment

---
+++ src/environment/environment.dev.ts
@@ -0,0 +1,25 @@
+export interface environments {
+
+    production: boolean;
+
+    api: {
+        baseUrl: 'http://localhost:3000'
+        version: 'v1'
+    }
+
+    auth: {
+        loginUrl: '/auth/login',
+
+    }
+
+    app: {
+        name: 'TechFix Front'
+
+    }
+
+
+
+
+
+
+} 
